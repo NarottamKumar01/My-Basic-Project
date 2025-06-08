99cb24bacc7f1d5d5248dfe2791396deb1a822ed
Narottam <Narottam.bxr8845@gmail.com>
2025-06-08 22:21:55 +0530

    Renamed directory

---
+++ QR_CODE_GEN/script.js
@@ -0,0 +1,16 @@
+let imgBox = document.getElementById("imgBox");
+let qrImage = document.getElementById("qrImage");
+let qrText = document.getElementById("qrText");
+function generateQR(){
+    if(qrText.value.length > 0){
+        qrImage.src = "https://quickchart.io/qr?text=" + qrText.value;
+        imgBox.classList.add("show-img");
+
+    }else{
+        qrText.classList.add('error');
+        setTimeout(()=>{
+            qrText.classList.remove('error');
+        },1000);
+    }
+
+}
